1d79397b7d01bab0bf2ef3bc92938af37226283f
Manel Baradad <mbaradad@mit.edu>
2022-05-02 14:28:15 -0400

    Repaired mixed-4 download link

--- scripts/download_pretrained_models/download_large_scale_encoders.sh
+++ scripts/download_pretrained_models/download_large_scale_encoders.sh
@@ -5,13 +5,16 @@ mkdir -p encoders/small_scale
 datasets=('dead_leaves-mixed' \
           'feature_vis-dead_leaves'
           'stat-spectrum_color_wmm' \
-          'stylegan-oriented' \
-          'mixed-4')
+          'stylegan-oriented')
 
 for DATASET in ${datasets[@]}
 do
     echo "Downloading pretrained model for dataset $DATASET"
     mkdir -p encoders/large_scale/$DATASET
     wget -O encoders/large_scale/$DATASET/checkpoint_0199.pth.tar http://data.csail.mit.edu/noiselearning/encoders/large_scale/$DATASET/checkpoint_0199.pth.tar
-
 done
+
+DATASET=mixed-4
+echo "Downloading pretrained model for dataset $DATASET"
+mkdir -p encoders/large_scale/$DATASET
+wget -O encoders/large_scale/$DATASET/checkpoint_0799.pth.tar http://data.csail.mit.edu/noiselearning/encoders/large_scale/$DATASET/checkpoint_0799.pth.tar
